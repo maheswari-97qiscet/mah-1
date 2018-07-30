17b703bda6df76952f58f1400f5db13bf5a5aa14
maheswari-97qiscet <41797942+maheswari-97qiscet@users.noreply.github.com>
2018-07-30 13:01:19 +0530

    Create AorC.java

---
+++ AorC.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class AorC
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	 Scanner sc=new Scanner(System.in);
+	 char c=sc.next().charAt(0);
+	 if(c=='a'||c=='e'|c=='i'||c=='o'||c=='u')
+	 {
+	 	System.out.println("vowel");
+	 }
+	 else if(c>='a'&&c>='z'||c>='A'&&c>='Z')
+	{
+	System.out.println("consonant");
+            }
+	else		
+	{
+	System.out.println("invalid");
+	}
+	
+	}
+}
