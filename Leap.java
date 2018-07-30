cfb498e471d3b21238a71563e4b7485eacce3676
maheswari-97qiscet <41797942+maheswari-97qiscet@users.noreply.github.com>
2018-07-30 20:39:48 +0530

    Create Leap.java

---
+++ Leap.java
@@ -0,0 +1,23 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Leap
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+Scanner	sc=new	Scanner(System.in);
+int	n=sc.nextInt();
+if(n%4==0)
+{
+	System.out.println("Yes");
+}
+else
+{
+	System.out.println("No");
+}
+	}
+}
