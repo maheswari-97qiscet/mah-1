22d4b82452b8a6953df720287bc704a165575c18
maheswari-97qiscet <41797942+maheswari-97qiscet@users.noreply.github.com>
2018-08-01 14:49:43 +0530

    Update PrimeInterval.java

--- PrimeInterval.java
+++ PrimeInterval.java
@@ -5,28 +5,33 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class PrimeInterval
+class Ideone
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		Scanner sc=new Scanner(System.in);
 		int a=sc.nextInt();
 		int b=sc.nextInt();
-                        while(a<b)
-                        {
-		boolean flag=false;                       
-                        for(int i=2;i<a/2;i++)
-                        {
-                        	if(a%i==0)
+		int count=0;
+                        for(int i=a;i<=b;i++)
+            	{
+                        	for(int j=2;j<i;j++)
                         	{
-                        	flag=true;
-                        	break;
+                        		if(i%j==0)
+                        		{
+                        		count=0;
+                        		break;
+            			}
+            			else
+            			{
+            			count=1;	
+            			}
                         	}
-                        }
-                        if(!flag)
-                        
-                        	System.out.print(a + "\t");
-                        	++a;
+            			if(count==1)
+                        		{
+                        		System.out.print( i + "\t");
+                        		}
+                        	
                        }
            	}
 }
