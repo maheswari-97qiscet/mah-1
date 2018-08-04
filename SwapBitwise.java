e2469871acc35ea8b43a3d4b093205199db397e1
maheswari-97qiscet <41797942+maheswari-97qiscet@users.noreply.github.com>
2018-08-04 14:03:52 +0530

    Create SwapBitwise.java

---
+++ SwapBitwise.java
@@ -0,0 +1,20 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class SwapBitwise
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner sc=new Scanner(System.in);
+	int a=sc.nextInt();
+	int b=sc.nextInt();
+	a=a^b;
+	b=a^b;
+	a=a^b;
+	System.out.print(a+" "+b);
+	}
+}
