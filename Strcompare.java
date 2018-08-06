0635b7b542ebf25188e5bf8d42070458bce6ec59
maheswari-97qiscet <41797942+maheswari-97qiscet@users.noreply.github.com>
2018-08-06 15:54:08 +0530

    Create Strcompare.java

---
+++ Strcompare.java
@@ -0,0 +1,31 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Strcompare
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+			Scanner sc=new Scanner(System.in);
+		String c=sc.next();
+		String c1=sc.next();
+		if(c.length()>c.length())
+		{
+			System.out.println(c);
+		}
+		else if(c.length()<c.length())
+		{
+		System.out.println(c1);
+		}
+		else
+		{
+		System.out.println(c1);
+		}
+
+	
+	}
+}
