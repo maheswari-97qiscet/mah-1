d3a786ea0ead0be5ecfe3f9017d1b31c7cc46fc9
maheswari-97qiscet <41797942+maheswari-97qiscet@users.noreply.github.com>
2018-08-09 11:00:56 +0530

    Update and rename SpecialCount.java to SpecialCount1.java

--- SpecialCount1.java
+++ SpecialCount1.java
@@ -1,24 +1,28 @@
 /* package whatever; // don't place package name! */
 
+/* package whatever; // don't place package name! */
+
 import java.util.*;
 import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class SpecialCount
+class SpecialCount1
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
+		// your code goes here
 		int count=0,i;
 		Scanner sc=new Scanner(System.in);
 		String s=sc.nextLine();
 		for(i=0;i<s.length();i++)
 		{
-			if(s.charAt(i)=='*'||s.charAt(i)=='^'||s.charAt(i)==' '||s.charAt(i)=='$'|s.charAt(i)=='@')
+			if(!(Character.isDigit(s.charAt(i)))&&(!(Character.isAlphabetic(s.charAt(i))))&&(!(s.charAt(i)=='.')))
 			{
 				count++;
 			}
 		}
 		System.out.println(count);
+
 	}
 }
