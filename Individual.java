896677a5543613044b9dd99e6f0fa4eeeb797e0f
maheswari-97qiscet <41797942+maheswari-97qiscet@users.noreply.github.com>
2018-08-10 20:19:08 +0530

    Create Individual.java

---
+++ Individual.java
@@ -0,0 +1,21 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Individual
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		String s=sc.next();
+		int i;
+		for(i=1;i<=s.length();i++)
+		{
+			System.out.print(i+" ");
+		}
+	}
+}
