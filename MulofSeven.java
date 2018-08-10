83f1dd18467bd0ca4676c7e68e90e29228a9f549
maheswari-97qiscet <41797942+maheswari-97qiscet@users.noreply.github.com>
2018-08-10 20:45:49 +0530

    Create MulofSeven.java

---
+++ MulofSeven.java
@@ -0,0 +1,24 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class MulofSeven
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int n1=sc.nextInt();
+		if(n1%7==0)
+		{
+			System.out.print("yes");
+		}
+		else
+		{
+		System.out.print("no");
+		}
+	}
+}
