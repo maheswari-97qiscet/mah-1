8925a703fd85e508fdae504d9d44e2ad98d188f0
maheswari-97qiscet <41797942+maheswari-97qiscet@users.noreply.github.com>
2018-08-13 21:11:10 +0530

    Create Area2.java

---
+++ Area2.java
@@ -0,0 +1,19 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Area2
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int n1=sc.nextInt();
+		int n2=sc.nextInt();
+		double res=n1*n2;	
+		 System.out.format("%.5f", res);
+	}
+}
