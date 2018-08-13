543fd8fafd7f905c00d8dfe65a92fc099714e8cd
maheswari-97qiscet <41797942+maheswari-97qiscet@users.noreply.github.com>
2018-08-13 21:16:35 +0530

    Update Area2.java

--- Area2.java
+++ Area2.java
@@ -5,15 +5,16 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class Area2
+class Ideone
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
-		Scanner sc=new Scanner(System.in);
-		int n1=sc.nextInt();
-		int n2=sc.nextInt();
+				Scanner sc=new Scanner(System.in);
+		float n1=sc.nextFloat();
+		 float n2=sc.nextFloat();
 		double res=n1*n2;	
 		 System.out.format("%.5f", res);
+	
 	}
 }
