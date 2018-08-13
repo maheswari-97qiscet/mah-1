178e0c67af522d5cdfeacbe967cf668c73db715f
maheswari-97qiscet <41797942+maheswari-97qiscet@users.noreply.github.com>
2018-08-13 12:07:32 +0530

    Create Decimal.java

---
+++ Decimal.java
@@ -0,0 +1,18 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Decimal
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		float f=sc.nextFloat();
+		int a=(int)(f+0.5);
+		System.out.println(a);
+	
+	}
+}
