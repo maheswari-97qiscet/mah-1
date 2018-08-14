18a420dc61160ad9be35a14e7731374a9c187602
maheswari-97qiscet <41797942+maheswari-97qiscet@users.noreply.github.com>
2018-08-14 12:48:50 +0530

    Create Gcd.java

---
+++ Gcd.java
@@ -0,0 +1,26 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Gcd
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int c=0,i;
+		for(i=1;i<=a&&i<=b;i++)
+		{
+			if(a%i==0&&b%i==0)
+			{
+				c=i;
+			}
+		}
+		System.out.println(c);
+	}
+}
