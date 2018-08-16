8aeedccba9ee7464d5cfcae5ea65ea145a3326ee
maheswari-97qiscet <41797942+maheswari-97qiscet@users.noreply.github.com>
2018-08-16 12:01:13 +0530

    Create NumConcate.java

---
+++ NumConcate.java
@@ -0,0 +1,18 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class NumConcate
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		String s1=sc.next();
+		String s2=sc.next();
+		System.out.print(s1+s2);
+	}
+}
