41724b574992a1e1552587abe08dc30a9876eb28
maheswari-97qiscet <41797942+maheswari-97qiscet@users.noreply.github.com>
2018-08-20 12:14:20 +0530

    Create Isogram.java

---
+++ Isogram.java
@@ -0,0 +1,32 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Isogram
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		String s1=sc.next();
+		String s2=sc.nextLine();
+		int i,count=0;
+		for(i=0;i<(s1.length()-1)&&i<(s2.length()-1);i++)
+		{
+			if(s1.charAt(i)==s1.charAt(i+1)||s2.charAt(i)==s2.charAt(i+1))
+			{
+				count=1;
+			}
+		}
+		if(count==1)
+		{
+			System.out.print("yes");
+		}
+		else
+		{
+			System.out.print("no");
+		}
+	}
+}
