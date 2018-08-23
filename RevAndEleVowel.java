27e49bb6e33bb30ff351e9d6ae4bbe88a0844f74
maheswari-97qiscet <41797942+maheswari-97qiscet@users.noreply.github.com>
2018-08-23 12:16:35 +0530

    Update RevAndEleVowel.java

--- RevAndEleVowel.java
+++ RevAndEleVowel.java
@@ -1,18 +1,22 @@
 /* package whatever; // don't place package name! */
 
+/* package whatever; // don't place package name! */
+
 import java.util.*;
 import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class RevAndEleVowel
+class Ideone
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
 		String s=sc.next();
 		String s1="",s2;
-		for(int i=0;i<s.length();i++)
+		n=s.length();
+		for(int i=0;i<n;i++)
 		{
 			s1=s.charAt(i)+s1;
 		}
