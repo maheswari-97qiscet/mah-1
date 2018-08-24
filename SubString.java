6f120dbbd6ce0bc15db79aa82b11d8d6e18bd608
maheswari-97qiscet <41797942+maheswari-97qiscet@users.noreply.github.com>
2018-08-24 21:05:10 +0530

    Create SubString.java

---
+++ SubString.java
@@ -0,0 +1,26 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class SubString
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		String s1=sc.next();
+		String s2=sc.next();
+		if(s1.contains(s2))
+		{
+			System.out.print("yes");
+		}
+		else
+		{
+			System.out.print("no");
+		}
+		
+		 	
+	}
+}
