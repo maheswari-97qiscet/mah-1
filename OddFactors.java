cbf7afe4f8c83692be0273ca4107dd4c17f469d8
maheswari-97qiscet <41797942+maheswari-97qiscet@users.noreply.github.com>
2018-08-25 20:04:07 +0530

    Create OddFactors.java

---
+++ OddFactors.java
@@ -0,0 +1,30 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class OddFactors
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int i;
+		for(i=1;i<=a;i++)
+		{
+			if(a%i==0)
+			{
+				if(i%2!=0)
+				{
+				if(i==a-1)
+				{
+				System.out.print(i);	
+				}
+				System.out.print(i+" ");
+				}
+			}
+		}
+	}
+}
