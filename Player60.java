f97d06eb50e41cddf83603e32d0536065aa19f20
maheswari-97qiscet <41797942+maheswari-97qiscet@users.noreply.github.com>
2018-08-29 12:33:19 +0530

    Create Player60.java

---
+++ Player60.java
@@ -0,0 +1,35 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Player60
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		String s1=sc.next();
+		String s2=sc.next();	
+		int i,count=0;
+		if(s1.length()==s2.length())
+		{
+		for(i=0;i<s1.length();i++)
+		{
+			if(s1.charAt(i)==s2.charAt(i))
+			{
+				count=1;
+			}
+		}}
+		if(count==1)
+		{
+		System.out.print("yes");
+		}
+		else
+		{
+		System.out.print("no");
+		}
+
+	}
+}
