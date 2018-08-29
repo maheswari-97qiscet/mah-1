bf96d6c9d6457565afba6269446691370fc3d914
maheswari-97qiscet <41797942+maheswari-97qiscet@users.noreply.github.com>
2018-08-29 20:22:01 +0530

    Update Player61.java

--- Player61.java
+++ Player61.java
@@ -7,28 +7,40 @@ import java.io.*;
 /* Name of the class has to be "Main" only if the class is public. */
 class Player61
 {
-	public static void main (String[] args) tahrows java.lang.Exception
+	public static void main (String[] args) throws java.lang.Exception
 	{
 		Scanner sc=new Scanner(System.in);
 		int n=sc.nextInt();
 		int k=sc.nextInt();
 		int a[]=new int[n];
-		int i,res=0;
+		int i,res=0,j,count=0;
 		for(i=0;i<n;i++)
 		{
 			a[i]=sc.nextInt();
 		}
 		for(i=0;i<n;i++)
 		{
-			res=res+a[i];
-		}
+		for(j=i;j<n;j++)
+			{
+			res=a[i]+a[j];
 		if(res==k)
 		{
-			System.out.print("yes");
+		count=1;
+		break;
+		}
+		}
+		if(count==1)
+		break;
+
+		}
+		if(count==1)
+		{
+		System.out.print("yes");	
 		}
 		else
 		{
 			System.out.print("no");
 		}
+		
 	}
 }
