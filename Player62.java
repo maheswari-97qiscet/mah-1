4856ae7e01f955dc962ad32f90408ee92ed2316d
maheswari-97qiscet <41797942+maheswari-97qiscet@users.noreply.github.com>
2018-08-29 20:57:41 +0530

    Create Player62.java

---
+++ Player62.java
@@ -0,0 +1,30 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Player62
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int i=1,x;
+		while(i>0)
+		{
+			if(n%i==0)
+			{
+			x=n/i;
+			if(x%2==1)
+			{a
+			System.out.print(i);
+			break;
+			}
+			}
+			i++;
+		}
+
+	}
+}
