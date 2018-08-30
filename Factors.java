da1a08fff68f54163d6916ec284690c7ddd2a6fc
maheswari-97qiscet <41797942+maheswari-97qiscet@users.noreply.github.com>
2018-08-30 10:39:17 +0530

    Update Factors.java

--- Factors.java
+++ Factors.java
@@ -5,11 +5,10 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class Factors
+class Factors.java
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		// your code goes here
 		Scanner sc=new Scanner(System.in);
 		int n=sc.nextInt();
 		int i;
@@ -17,8 +16,14 @@ class Factors
 		{
 			if(n%i==0)
 			{
-				System.out.print(i+" ");
+				if(i==n)
+				{
+				System.out.print(i);
+				}
+				else
+				{
+					System.out.print(i+" ");
+				}
 			}
-		}
-	}
+		}	}
 }
