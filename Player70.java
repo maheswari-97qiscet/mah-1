1bf274cdc4e1bc5392154ef284cd02055616c10d
maheswari-97qiscet <41797942+maheswari-97qiscet@users.noreply.github.com>
2018-08-30 11:33:03 +0530

    Create Player70.java

---
+++ Player70.java
@@ -0,0 +1,18 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Player70
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		String s1=sc.next();
+		String s2="Answer";
+		System.out.print(s1+" "+s2);
+		
+	}
+}
