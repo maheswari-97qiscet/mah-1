6f67ecbb21b095e6b834488f8ccfe16ec05b47c5
maheswari-97qiscet <41797942+maheswari-97qiscet@users.noreply.github.com>
2018-08-30 12:31:29 +0530

    Create Player75.java

---
+++ Player75.java
@@ -0,0 +1,34 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Player75
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int a[]=new int[n];
+		int i,j,count=0;
+		for(i=0;i<n;i++)
+		{
+			a[i]=sc.nextInt();
+		}
+		for(i=0;i<n;i++)
+		{
+			for(j=i+1;j<n;j++)
+			{
+				if(i<j)
+				{
+					count++;
+				}
+			}
+			
+		}
+		System.out.print(count);
+	}
+}
