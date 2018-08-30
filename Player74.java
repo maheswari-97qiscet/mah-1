b7783127635d859eb2c1c6c19bbc1cfc97889df5
maheswari-97qiscet <41797942+maheswari-97qiscet@users.noreply.github.com>
2018-08-30 12:19:07 +0530

    Create Player74.java

---
+++ Player74.java
@@ -0,0 +1,19 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Player74
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int k=sc.nextInt();
+		int c=(int)Math.pow(n,k);
+		System.out.print(c);
+		}
+}
