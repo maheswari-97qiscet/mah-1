e898e055493d0780eac8a8e6c64594bfc85aec0d
maheswari-97qiscet <41797942+maheswari-97qiscet@users.noreply.github.com>
2018-09-12 13:17:21 +0530

    Create Huntur3.java

---
+++ Huntur3.java
@@ -0,0 +1,36 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+			Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int i;
+		int a[]=new int[n];
+		for(i=0;i<n;i++)
+		{
+			a[i]=sc.nextInt();
+		}
+		for(i=0;i<n;i++)
+		{
+		if(i==a[i])
+		{
+			if(i==n-1)
+			{
+			System.out.print(a[i]);
+			}
+			else
+			{
+				System.out.print(a[i]+" ");
+			}
+		}
+		}
+	
+	}
+}
