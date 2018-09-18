f3b66351e28c8785c071897cb3f488bf7a36dc4c
maheswari-97qiscet <41797942+maheswari-97qiscet@users.noreply.github.com>
2018-09-18 14:14:14 +0530

    Create Player49.java

---
+++ Player49.java
@@ -0,0 +1,24 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Player49
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		if(n>=(-2147483648)||n<=(2147438647))
+		{
+			System.out.print("INT");
+		}
+		else
+		{
+			System.out.print("LONG");
+		}
+
+	}
+}
